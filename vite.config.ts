d99750157e60e49820bd8efcd24207a39d743d71
Nathan Szadowski <nszadowski@gmail.com>
2024-12-03 14:23:45 -0500

    vite config edit

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'aws-amplify': ['aws-amplify'],
+          'aws-amplify-react': ['@aws-amplify/ui-react'],
+          'react-dom': ['react-dom/client'],
+        },
+      },
+    },
+  },
 })
