faa5008fae45cc88d0563771779e4b66ec1a8679
Nathan Szadowski <nszadowski@gmail.com>
2024-12-09 15:10:04 -0500

    changed config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,48 @@
-import { defineConfig, splitVendorChunkPlugin } from "vite";
-import react from "@vitejs/plugin-react";
-import svgr from "vite-plugin-svgr";
-import sass from "sass";
-import path from "path";
-const resolvePath = (str: string) => path.resolve(__dirname, str);
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), svgr()],
-  // Configure the necessary loaders
-  css: {
-    preprocessorOptions: {
-      scss: {
-        implementation: sass,
-        sassOptions: {
-          includePaths: ["./node_modules"],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+  plugins: [
+    react(),
+    differMuiSourcemapsPlugins(),
+  ],
+  build: {
+    sourcemap: mode === "development" || "hidden",
+    rollupOptions: {
+      maxParallelFileOps: 20,
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+        sourcemapIgnoreList: (relativeSourcePath) => {
+          const normalizedPath = path.normalize(relativeSourcePath);
+          return normalizedPath.includes("node_modules");
         },
       },
+      cache: false,
     },
   },
-  resolve: {
-    alias: {
-      "@": resolvePath("src"),
-    },
-  },
-
-  server: {
-    port: 3000,
-  },
-  define: {
-    "process.env": {},
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolvePath("index.html"),
-        legacy: resolvePath("index.html"),
-      },
-    },
-  },
+};
 });
+
+function differMuiSourcemapsPlugins() {
+  const muiPackages = ["@mui/material", "@emotion/styled", "@emotion/react"];
+
+  return {
+    name: "differ-mui-sourcemap",
+    transform(code: string, id: string) {
+      if (muiPackages.some((pkg) => id.includes(pkg))) {
+        return {
+          code: code,
+          map: null,
+        };
+      }
+    },
+  };
+}
